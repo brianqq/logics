980ab2a8432295c4547d0fa5a2d280427655f22d
Brian Levy <brian_levy@brown.edu>
2015-06-22 16:28:40 -0400

    Fixed logic in def-trans. Added notes on macros.

--- first-order-res.lisp
+++ first-order-res.lisp
@@ -33,10 +33,11 @@
     ((type list) (mapcar #'proc-skel skel))
     ((satisfies atom) skel)))
 
-(defmacro def-trans (name &body rules)
-  "A more concise way to define a pattern-matching transformatioen"
+(defmacro def-trans (name docstring &body rules)
+  "a domain specific language for defining pattern-matching transformations"
   (let ((sentence (gensym "sentence")))
     `(defun ,name (,sentence) 
+       ,docstring
        (match ,sentence
 	 ,@(iter (for (ptrn skel) in rules)
 		 (collect (list (proc-pattern ptrn)
@@ -45,7 +46,8 @@
 				   ,(proc-skel skel)))))
 	 (_ ,sentence)))))
 
-(defmacro def-trans-comp (name &body parts)
+(defmacro def-trans-comp (name docstring &body parts)
+  "Defines a composition of transformers"
   (let ((sentence (gensym "sentence")))
     `(defun ,name (,sentence)
        ,(reduce (lambda (queue next)
@@ -54,6 +56,7 @@
 		:initial-value sentence))))
 
 (def-trans simplify
+  "some basic boolean algebra simplifications"
   ((:not (:not x)) x)
   ((:and 0 _) 0)
   ((:and _ 0) 0)
@@ -61,20 +64,37 @@
   ((:or x 0) x)
   ((:or _ 1) 1)
   ((:or 1 _) 1))
+;;; the colon prefix makes a keyword.
+;;; this is the same as a symbol, except keywords are self-evaluating
+;;; so > :not => :not, whereas > 'not => not. I was getting a headache
+;;; requoting everything each time it's processed.
 
 (def-trans expand-if
+  "expands if and iff into equivalent statements using and, or, not"
   ((:if a b) (:or (:not a) b))
   ((:iff a b) (:and (:or (:not a) b) (:or a (:not b)))))
+;;; equivalently, this could be defined as the function 
+#|
+(defun expand-if (sentence)
+  "expands if and iff into equivalent statements using and, or, not" 
+  (match sentence
+    ((list :if a b) `(:or (:not ,(expand-if a) ,(expand-if b))))
+    ((list :iff a b) `(:and (:or (:not ,(expand-if a)) ,(expand-if b))
+			    (:or ,(expand-if a) (:not ,(expand-if b)))))))
+|#
 
 (def-trans de-morgan
+  "uses de morgan's law as a step in converting to negative normal form"
   ((:not (:or a b)) (:and (:not a) (:not b)))
   ((:not (:and a b)) (:or (:not a) (:not b))))
 
 (def-trans-comp neg-norm-form
+  "converts to negative normal form"
   expand-if
   de-morgan)
 
 (defun subs (sentence var val)
+  "makes a variable substitution"
   ;should this eventually handle skolem function + predicate substitutions?
   (match sentence
     ((list 'skolem fn subsent) (list 'skolem fn (substitute subsent var val))) 
@@ -84,13 +104,15 @@
     (_ sentence)))
 
 (def-trans unique-binds
+  "replaces bound variables with unique names"
   ((:forall x stuff) (subs (:forall x stuff) x (gensym (string x)))))
+;;; gensym might break things. If it does, then I'll roll my own.
 
 (def-trans skolemize-1
-  ((and a (:forall b stuff)) (:forall b (and a stuff)))
-  ((or a (:forall b stuff)) (:forall b (or a stuff)))
-  ((and a (:exists b stuff)) (:exists b (and a stuff)))
-  ((or a (:exists b stuff)) (:exists b (or a stuff))))
+  ((:and a (:forall b stuff)) (:forall b (and a stuff)))
+  ((:or a (:forall b stuff)) (:forall b (or a stuff)))
+  ((:and a (:exists b stuff)) (:exists b (and a stuff)))
+  ((:or a (:exists b stuff)) (:exists b (or a stuff))))
 
 (defun conj-norm-form (sentence)
 )
